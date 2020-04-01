85483a891fffef21c794f99e2f9603365a413364
Andre Sencioles <asenci@gmail.com>
2020-04-02 12:14:19 +1300

    Add homebrew formula

---
+++ contrib/libudt.rb
@@ -0,0 +1,15 @@
+class Libudt < Formula
+  desc "UDT is a reliable UDP based application level data transport protocol for distributed data intensive applications over wide area high-speed networks"
+  homepage "http://udt.sf.net"
+  url "https://github.com/asenci/udt/archive/v4.11.tar.gz"
+  version "4.11"
+  sha256 "af5ffc8d8d968e4d9e431f46cbd80b791cebc9c5e6e1bc67f6accb8ce71e5c5c"
+
+  def install
+    Dir.chdir('src')
+    system "make -e os=OSX arch=AMD64"
+    lib.install "libudt.a"
+    lib.install "libudt.dylib"
+    include.install "udt.h"
+  end
+end
